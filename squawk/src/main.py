74cea5fa17a981e715e7b3b0555d2d91aa56e6a0
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-18 18:26:18 -0500

    update reading

--- squawk/src/main.py
+++ squawk/src/main.py
@@ -10,17 +10,6 @@ app = Flask(__name__)
 def venues():
     conn = psycopg2.connect(database = 'foursquare_development', user = 'postgres', password = 'postgres')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM venues;')
-    venues = cursor.fetchall()
-    venue_objs = [Venue(venue).__dict__ for venue in venues]
-    return jsonify(venue_objs)
-
-@app.route('/venues/<id>')
-def show_venue(id):
-    conn = psycopg2.connect(database = 'foursquare_development', user = 'postgres', password = 'postgres')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM venues WHERE id = %s LIMIT 1;", id)
-    venue_values = cursor.fetchone()
-    return jsonify(Venue(venue_values).__dict__)
+    pass    
 
 app.run(debug = True)
