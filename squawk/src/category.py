e5383cc5622f1e952ab66c72ed302337670636ca
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-18 18:27:22 -0500

    update code

--- squawk/src/category.py
+++ squawk/src/category.py
@@ -1,7 +1,10 @@
 class Category:
     __table__ = 'categories'
     columns = ['id', 'name']
-    def __init__(self, values):
-        self.__dict__ = None
-
-
+    
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in columns: {self.columns}')
+        for k, v in kwargs.items():
+            setattr(self, k, v)
--- squawk/src/venue.py
+++ squawk/src/venue.py
@@ -3,7 +3,11 @@ class Venue():
     columns = ['id', 'foursquare_id', 'name', 'price',
             'rating', 'likes', 'menu_url']
 
-    def __init__(self, values):
-        self.__dict__ = dict(zip(self.columns, values))
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in columns: {self.columns}')
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
 
